2e99f8c36d6e618121ed101dbf6d5c743c04e334
efikhan <135870653+efikhan@users.noreply.github.com>
2026-02-26 14:32:49 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+```bash
+#!/bin/bash
+cd /opt
+rm -rf truma 2>/dev/null
+mkdir truma
+cd truma
+wget https://raw.githubusercontent.com/efikhan/Truma-Tunnel/main/truma.sh
+wget https://raw.githubusercontent.com/efikhan/Truma-Tunnel/main/gre-manager.sh
+wget https://raw.githubusercontent.com/efikhan/Truma-Tunnel/main/paqet.sh
+wget https://raw.githubusercontent.com/efikhan/Truma-Tunnel/main/mesh-manager.sh
+wget https://raw.githubusercontent.com/efikhan/Truma-Tunnel/main/haproxy-manager.sh
+chmod +x *.sh
+sed -i 's/\r$//' *.sh
+./truma.sh
+```
